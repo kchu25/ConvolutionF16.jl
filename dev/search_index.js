eb34863917d16745ce84e67157fa4bb02a69b324
Documenter.jl <documenter@juliadocs.github.io>
2024-01-24 22:24:04 +0000

    build based on 8e31d31

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConvolutionF16","category":"page"},{"location":"#ConvolutionF16","page":"Home","title":"ConvolutionF16","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConvolutionF16.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConvolutionF16]","category":"page"}]
+}
